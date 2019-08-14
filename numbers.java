fa44247fce11e3657bfe178c60cca7fa037086f3
Halima2427 <53898536+Halima2427@users.noreply.github.com>
2019-08-14 16:32:23 +0530

    Add files via upload

---
+++ numbers.java
@@ -0,0 +1,16 @@
+class numbers
+{
+    public static void main(String args[])
+    {
+        int val=10;
+        if(val>=0)
+        {
+            System.out.println(val+"is a Positive number");
+        }
+        else
+        {
+          System.out.println(val+"is a Negative number");
+
+        }
+    }
+}
