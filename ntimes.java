cd12ab8f30b90052589548012947820338598ba4
Halima2427 <53898536+Halima2427@users.noreply.github.com>
2019-08-15 22:15:37 +0530

    Add files via upload

---
+++ ntimes.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class ntimes
+{
+    public static void main(String args[])
+    {
+        Scanner sc5=new Scanner(System.in);
+        String s=sc5.nextLine();
+        int n=Integer.parseInt(s);
+        for(int i=1; i<n; i++)
+        {
+            System.out.println("hello");
+        }
+
+    }
+}
