6a793591953a618b233d0de87f29a26fca8eb4aa
Halima Begam <53898536+Halima2427@users.noreply.github.com>
2019-08-17 13:41:33 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,14 @@
+class power
+{
+    public static void main (String args[])
+    {
+        int b=3,e=3;
+       long data=1;
+       while(e!=0)
+       {
+           data=data*b;
+           --e;
+       }
+       System.out.println("The value of exponential power is:"+data);
+    }
+}
